0913c5f6169ff585b22782e965f1d9e26dd5c385
kuldeep-singh <kuldeep.singh@dream11.com>
2026-01-20 18:53:39 +0530

    multiline support

--- deploy/healthcheck-dummy/generate-healthcheck-logs.sh
+++ deploy/healthcheck-dummy/generate-healthcheck-logs.sh
@@ -8,9 +8,20 @@ HEALTH_CHECK_LOG_FILE=/var/log/healthcheck/healthcheck.log
 # Create log directory if it doesn't exist
 mkdir -p /var/log/healthcheck
 
-while true; do
+generateMultilineLog() {
     echo "$(date '+%Y-%m-%d %H:%M:%S') - This is a healthcheck log entry $i" >> "$HEALTH_CHECK_LOG_FILE"
-    sleep 1
+    echo "This is a second line of the log entry" >> "$HEALTH_CHECK_LOG_FILE"
+    echo "This is a third line of the log entry" >> "$HEALTH_CHECK_LOG_FILE"
+}
+
+while true; do
+    # Every 100th log, generate a multiline stack trace
+    if (( i % 100 == 0 )); then
+            generateMultilineLog
+    else
+        echo "$(date '+%Y-%m-%d %H:%M:%S') - This is a healthcheck log entry $i" >> "$HEALTH_CHECK_LOG_FILE"
+    fi
+    sleep 0.01
     ((i++))
 done
 
